43d1ebac723c127be1291fa75381e928dc13c943
ckyrouac <ckyrouac@redhat.com>
2023-01-24 10:06:47 -0500

    [ESSNTL-1109] parse db connection from environment variables

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	. "github.com/RedHatInsights/xjoin-validation/internal/database"
 	. "github.com/RedHatInsights/xjoin-validation/internal/elasticsearch"
 	. "github.com/RedHatInsights/xjoin-validation/internal/validator"
+	"github.com/go-errors/errors"
 	"os"
 	"strings"
 	"time"
@@ -32,6 +33,40 @@ type Config struct {
 	FullAvroSchema        string `config:"FULL_AVRO_SCHEMA"`
 }
 
+func parseDatabaseConnectionFromEnv(datasourceName string) (dbConnectionInfo DatabaseConnectionInfo, err error) {
+	dbConnectionInfo.Hostname = os.Getenv(datasourceName + "_DB_HOSTNAME")
+	dbConnectionInfo.Username = os.Getenv(datasourceName + "_DB_USERNAME")
+	dbConnectionInfo.Password = os.Getenv(datasourceName + "_DB_PASSWORD")
+	dbConnectionInfo.Name = os.Getenv(datasourceName + "_DB_NAME")
+	dbConnectionInfo.Port = os.Getenv(datasourceName + "_DB_PORT")
+	dbConnectionInfo.Table = os.Getenv(datasourceName + "_DB_TABLE")
+	dbConnectionInfo.SSLMode = os.Getenv(datasourceName + "_DB_SSL_MODE")
+
+	if dbConnectionInfo.Hostname == "" {
+		return dbConnectionInfo, errors.Wrap(errors.New(
+			"missing database hostname environment variable for datasource: "+datasourceName), 0)
+	} else if dbConnectionInfo.Username == "" {
+		return dbConnectionInfo, errors.Wrap(errors.New(
+			"missing database username environment variable for datasource: "+datasourceName), 0)
+	} else if dbConnectionInfo.Password == "" {
+		return dbConnectionInfo, errors.Wrap(errors.New(
+			"missing database password environment variable for datasource: "+datasourceName), 0)
+	} else if dbConnectionInfo.Name == "" {
+		return dbConnectionInfo, errors.Wrap(errors.New(
+			"missing database name environment variable for datasource: "+datasourceName), 0)
+	} else if dbConnectionInfo.Port == "" {
+		return dbConnectionInfo, errors.Wrap(errors.New(
+			"missing database port environment variable for datasource: "+datasourceName), 0)
+	} else if dbConnectionInfo.Table == "" {
+		return dbConnectionInfo, errors.Wrap(errors.New(
+			"missing database table environment variable for datasource: "+datasourceName), 0)
+	} else if dbConnectionInfo.SSLMode == "" {
+		dbConnectionInfo.SSLMode = "disable"
+	}
+
+	return
+}
+
 //currently assumes a single reference
 func main() {
 	fmt.Println("Starting validation...")
@@ -41,14 +76,6 @@ func main() {
 	err := config.From("config/dev.config").FromEnv().To(&c)
 	if err != nil {
 		fmt.Println("error parsing config")
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	var dbConnectionInfo map[string]DatabaseConnectionInfo
-	err = json.Unmarshal([]byte(c.DatabaseConnections), &dbConnectionInfo)
-	if err != nil {
-		fmt.Println("error parsing DatabaseConnections json")
 		fmt.Println(err)
 		os.Exit(1)
 	}
@@ -65,15 +92,27 @@ func main() {
 	}
 
 	//connect to database
-	datasourceName := strings.Split(parsedSchema.FullAvroSchema.Namespace, ".")[0]
+	if len(strings.Split(parsedSchema.FullAvroSchema.Namespace, ".")) < 2 {
+		fmt.Println("Invalid FullAvroSchema.Namespace value. Expected '.' delimited string, e.g. xjoinindexpipeline.hosts.1")
+		os.Exit(1)
+	}
+
+	datasourceName := strings.Split(parsedSchema.FullAvroSchema.Namespace, ".")[1]
+	dbConnectionInfo, err := parseDatabaseConnectionFromEnv(datasourceName)
+	if err != nil {
+		fmt.Println("error parsing database connection from environment variables")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	dbClient, err := NewDBClient(DBParams{
-		User:             dbConnectionInfo[datasourceName].Username,
-		Password:         dbConnectionInfo[datasourceName].Password,
-		Host:             dbConnectionInfo[datasourceName].Hostname,
-		Name:             dbConnectionInfo[datasourceName].Name,
-		Port:             dbConnectionInfo[datasourceName].Port,
-		Table:            dbConnectionInfo[datasourceName].Table,
-		SSLMode:          dbConnectionInfo[datasourceName].SSLMode,
+		User:             dbConnectionInfo.Username,
+		Password:         dbConnectionInfo.Password,
+		Host:             dbConnectionInfo.Hostname,
+		Name:             dbConnectionInfo.Name,
+		Port:             dbConnectionInfo.Port,
+		Table:            dbConnectionInfo.Table,
+		SSLMode:          dbConnectionInfo.SSLMode,
 		ParsedAvroSchema: parsedSchema,
 	})
 	if err != nil {
